8eeebb1a4f1e844bb41e4f2c3657cf67a4c81fd9
Markus Wiegand <mail@morphy2k.dev>
2021-03-10 11:24:02 +0100

    update dependencies

--- lib/providers/item.dart
+++ lib/providers/item.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import '../models/items/ammunition.dart';
 import '../models/items/armor.dart';
